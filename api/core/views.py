bfc9fda3c1cefe8b01134ca3f4e42f26f948d903
Cloud <smirnovceo@yandex.ru>
2021-09-29 15:57:52 +0500

    added core/urls.py with route logic / added PostVeiwSet

--- api/core/views.py
+++ api/core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import PostSerialazer
+from .models import Post
+from rest_framework.response import Response
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerialazer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
